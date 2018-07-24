44ab9d41bffc9ba8261bf84ba9f599bd586c39a6
ManuelVU <jesus_mvu@hotmail.com>
2018-07-24 16:37:42 -0500

    adding first function and plots to results and src

--- src/Functions.R
+++ src/Functions.R
@@ -1,1 +1,9 @@
 # This file is for the functions used in the optimal experimental design project
+ellipsoid<-function(x1,x2){
+  R<-c()
+  for(i in 1:length(x1)){
+    o<-c(x1[i],x2[i])
+    R[i]<-(t(o-mu)%*%solve(mlvar)%*%(o-mu))
+  }
+  return(R)
+}
